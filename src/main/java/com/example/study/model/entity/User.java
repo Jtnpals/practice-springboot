045296eac6334ac86cab5d5a3caa999e94e036ca
jtnpals <jtnpals@gmail.com>
2019-09-07 00:27:38 +0900

    Add user entity

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity
+//@Table(name = "user") //동일하기 때문에 굳이 선언하지 않아도 됨
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//    @Column(name = "account") //동일하기 때문에 굳이 선언 안해도 됨
+    private String account;
+    private String email;
+    private String phoneNumber; //자동으로 camel case 를 snake case 로 변환해줌 굳이 phone_number 로 안해도됨
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
