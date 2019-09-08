f331db9aea727f46944cbf6e77a95649dc496d5f
jtnpals <jtnpals@gmail.com>
2019-09-09 05:38:13 +0900

    Add ItemRepositoryTest

---
+++ src/test/java/com/example/study/repository/ItemRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.Item;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class ItemRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void create(){
+        Item item = new Item();
+        item.setName("노트북");
+        item.setPrice(100000);
+        item.setContent("삼성 노트북");
+
+        Item newItem = itemRepository.save(item);
+        Assert.assertNotNull(newItem);
+    }
+
+    @Test
+    public void read(){
+        Long id = 1L;
+
+        Optional<Item> item = itemRepository.findById(id);
+        // :: 는 메서드 레퍼런스를 의미 i -> System.out.println(i);
+        item.ifPresent(System.out::println);
+
+    }
+}
