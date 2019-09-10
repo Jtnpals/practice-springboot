fc9d9aa1b64d623ce4681dbc6308bb53d87c1183
jtnpals <jtnpals@gmail.com>
2019-09-11 04:10:09 +0900

    Update repository

--- src/main/java/com/example/study/repository/UserRepository.java
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -8,8 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {//User 의 repository 이고 기본키가 Long 형
-    // select * from user where account = ?
-    Optional<User> findByAccount(String account);
-    Optional<User> findByEmail(String email);
-    Optional<User> findByAccountAndEmail(String account, String email);
 }
