a6c67a4e6f0196199db997a64adb42833f7b004c
jtnpals <jtnpals@gmail.com>
2019-09-19 06:50:13 +0900

    Add ItemApiCreate

--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -10,6 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,7 +19,8 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@ToString(exclude = {"orderDetailList", "partner"})@Builder
+@ToString(exclude = {"orderDetailList", "partner"})
+@Builder
 @Accessors(chain = true)
 public class Item {
 
@@ -29,7 +31,7 @@ public class Item {
     private String name;
     private String title;
     private String content;
-    private Integer price;
+    private BigDecimal price;
     private String brandName;
     private LocalDateTime registeredAt;
     private LocalDateTime unregisteredAt;
