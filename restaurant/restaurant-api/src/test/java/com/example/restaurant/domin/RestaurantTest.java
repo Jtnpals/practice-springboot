ba4bde7cffc5f5d8af8392a998139a479f834945
jtnpals <jtnpals@gmail.com>
2019-09-26 03:50:21 +0900

    Add RestaurantTest

--- restaurant/restaurant-api/src/test/java/com/example/restaurant/domin/RestaurantTest.java
+++ restaurant/restaurant-api/src/test/java/com/example/restaurant/domin/RestaurantTest.java
@@ -9,15 +9,16 @@ import static org.junit.Assert.*;
 public class RestaurantTest {
     @Test
     public void creation(){
-        Restaurant restaurant = new Restaurant("Restaurant", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Restaurant", "Seoul");
 
         assertThat(restaurant.getName(), is("Restaurant"));
         assertThat(restaurant.getAddress(), is("Seoul"));
+        assertThat(restaurant.getId(), is(1004L));
     }
 
     @Test
     public void information(){
-        Restaurant restaurant = new Restaurant("Restaurant", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L,"Restaurant", "Seoul");
 
         assertThat(restaurant.getInformation(), is("Restaurant in Seoul"));
     }
