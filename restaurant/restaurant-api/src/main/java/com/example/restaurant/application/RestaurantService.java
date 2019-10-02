e6911c0b3f8a9f513b22568aa13ad357e4222374
jtnpals <jtnpals@gmail.com>
2019-10-03 03:56:04 +0900

    Add MockCreate

--- restaurant/restaurant-api/src/main/java/com/example/restaurant/application/RestaurantService.java
+++ restaurant/restaurant-api/src/main/java/com/example/restaurant/application/RestaurantService.java
@@ -32,4 +32,7 @@ public class RestaurantService {
         List<Restaurant> restaurants = restaurantRepository.findAll();
         return restaurants;
     }
+
+    public void addRestaurant(Restaurant restaurant) {
+    }
 }
--- restaurant/restaurant-api/src/test/java/com/example/restaurant/interfaces/RestaurantControllerTest.java
+++ restaurant/restaurant-api/src/test/java/com/example/restaurant/interfaces/RestaurantControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,10 +16,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RestaurantController.class)
@@ -47,15 +50,28 @@ public class RestaurantControllerTest {
        restaurant.addMenuItem(new MenuItem("Kimchi"));
         given(restaurantService.getRestaurant(1004L)).willReturn(restaurant);
         given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
-        mvc.perform(get("/restaurant/1004"))
+        mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"name\":\"Bob zip\"")))
                 .andExpect(content().string(containsString("\"id\":1004")))
             .andExpect(content().string(containsString("Kimchi")));
 
-       mvc.perform(get("/restaurant/2020"))
+       mvc.perform(get("/restaurants/2020"))
                .andExpect(status().isOk())
                .andExpect(content().string(containsString("\"name\":\"Cyber food\"")))
                .andExpect(content().string(containsString("\"id\":2020")));
+   }
+
+   @Test
+    public void create() throws Exception {
+        mvc.perform(post("/restaurants")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"Beryon\",\"address\":\"Busan\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", "/restaurants/1234"))
+                .andExpect(content().string("{}"));
+
+        verify(restaurantService).addRestaurant(any());
+
    }
 }
