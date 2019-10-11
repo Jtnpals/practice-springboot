62f71b24245b5f9465aead55a5203930da3c856b
jtnpals <jtnpals@gmail.com>
2019-10-12 05:58:38 +0900

    Add updateMenu

---
+++ restaurant/restaurant-api/src/main/java/com/example/restaurant/application/MenuItemService.java
@@ -0,0 +1,14 @@
+package com.example.restaurant.application;
+
+
+import com.example.restaurant.domin.MenuItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
+    }
+}
