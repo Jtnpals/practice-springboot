e37bf030459def3823d0b991b0e3ad7eb67bef4f
jtnpals <jtnpals@gmail.com>
2019-10-16 04:28:35 +0900

    Add ReviewController

---
+++ restaurant/restaurant-api/src/main/java/com/example/restaurant/application/ReviewService.java
@@ -0,0 +1,11 @@
+package com.example.restaurant.application;
+
+import com.example.restaurant.domin.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public void addReview(Review review){
+
+    }
+}
