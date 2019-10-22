a8f280d70ee56490b2976f04cb17b118f3edfbb1
jtnpals <jtnpals@gmail.com>
2019-10-23 04:55:58 +0900

    Add application.yml

--- restaurant/admin-api/src/main/java/com/example/restaurant/interfaces/MenuItemController.java
+++ restaurant/admin-api/src/main/java/com/example/restaurant/interfaces/MenuItemController.java
@@ -3,10 +3,7 @@ package com.example.restaurant.interfaces;
 import com.example.restaurant.application.MenuItemService;
 import com.example.restaurant.domin.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,5 +18,10 @@ public class MenuItemController {
 
         menuItemService.bulkUpdate(restaurantId, menuItems);
         return "";
+    }
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> getMenuItems(@PathVariable Long restaurantId){
+        return menuItemService.getMenuItems(restaurantId);
     }
 }
